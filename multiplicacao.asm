8a73a698b6e651105561a96b255ff5e033eb2ddb
Jaine Senna <jainesantossenna191@gmail.com>
2019-08-18 15:12:14 -0300

    Firs Commit

---
+++ multiplicacao.asm
@@ -0,0 +1,32 @@
+.data
+	num1: .asciiz "Informe o primeiro numero: "
+	num2: .asciiz "Informe o segundo numero: "
+	resultado: .asciiz "O resultado da multiplicação é: "
+.text 
+	li $v0,4
+	la $a0,num1
+	syscall
+	
+	li $v0,5
+	syscall
+	
+	move $t0, $v0
+	
+	li $v0,4
+	la $a0,num2
+	syscall
+	
+	li $v0,5
+	syscall
+	
+	move $t1, $v0
+	
+	mul $t2, $t0, $t1
+	
+	li $v0,4
+	la $a0,resultado
+	syscall
+	
+	li $v0,1
+	move $a0, $t2
+	syscall
